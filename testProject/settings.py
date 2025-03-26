fe68482d2e91c2cc1e2704469b3b4a76d412b210
bulut <ahmetbulut@sehir.edu.tr>
2025-03-26 13:29:37 +0300

    dockerized. redis service added. postgres db backend service. two sample views are cache-supported.

--- testProject/settings.py
+++ testProject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,13 +75,33 @@ WSGI_APPLICATION = 'testProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '5432'
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://redis:6379',  # 'redis' is the service name in docker-compose
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- testProject/views.py
+++ testProject/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 
 from app1.models import M
 
+
+@cache_page(60)
 def index(request):
     return render(request, "base.html")
 
+@cache_page(60)
 def hello(request, category, id):
     if category == 'funky':
         raise Http404("Sorry !")
