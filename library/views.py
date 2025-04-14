27c68a052975e0fe1772b1a7cb67539634e91926
bulut <ahmetbulut@sehir.edu.tr>
2025-04-14 14:35:21 +0300

    Placeholder implementation for web form with Publisher objects.

--- library/views.py
+++ library/views.py
@@ -2,9 +2,15 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render
 
+from library.models import Publisher
+
+
 def home(request):
     return render(request, "home.html")
 
 def search(request):
-    return render(request, "results.html")
+    return render(request,
+                  "results.html",
+                  context = {'publishers': Publisher.objects.all()
+})
 
